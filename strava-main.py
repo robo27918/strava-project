b8b4028421dc5373dce358c03ff8b43bde8d4cd0
Your Name <roberttoribio04@gmail.com>
2026-02-12 17:14:06 -0800

    adds more changes

--- strava-main.py
+++ strava-main.py
@@ -1,14 +1,14 @@
 from dotenv import load_dotenv
 from StravaClientTokenService import StravaClientTokenService
 from pprint import pprint
-from Repository import Repository 
+# from Repository import Repository 
 load_dotenv()
 
 service = StravaClientTokenService('tokens.json')
 client = service.get_client()
 activities = client.get_activities()
 #create a repository for postgres
-BaseRepository("")
+# BaseRepository("")
 
 for a in activities:
    pprint(a.__str__())
