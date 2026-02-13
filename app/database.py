074e4e9dec050a02ead0c20a678b80585298bbee
Your Name <roberttoribio04@gmail.com>
2026-02-12 17:36:03 -0800

    adds how to get connection from SQLAlchemy tutorial

---
+++ app/database.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine,text
+engine = create_engine("sqlite+pysqlite:///:memory:",echo=True)
+
+# creating context manager for Engine
+with engine.connect() as conn:
+    result = conn.execute(text("select 'hello world'"))
+    print(result.all())
