cd25d5cb50f1deda1ecbb973ca33a90a4f4aa3c4
Your Name <roberttoribio04@gmail.com>
2026-02-12 17:46:40 -0800

    adds commiting changes

--- app/database.py
+++ app/database.py
@@ -3,5 +3,12 @@ engine = create_engine("sqlite+pysqlite:///:memory:",echo=True)
 
 # creating context manager for Engine
 with engine.connect() as conn:
-    result = conn.execute(text("select 'hello world'"))
-    print(result.all())
+    conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+    conn.execute(
+        text("INSERT INTO some_table (x,y) VALUES(:x, :y)"),
+             [{"x" :1 , "y":1},
+              {"x": 34, "y":25},
+            ]
+        )
+    
+    conn.commit()# commits it to the table 
