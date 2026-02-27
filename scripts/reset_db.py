e9323e481ff735cc31f03b70f1a87a2ec2217b4d
Your Name <roberttoribio04@gmail.com>
2026-02-26 22:01:33 -0800

    fixes issue with drop db

--- scripts/reset_db.py
+++ scripts/reset_db.py
@@ -1,11 +1,13 @@
 # scripts/reset_db.py
 from models.base import Base, engine
 from utils.db import init_db
-
+import models.summary_activity
+#this doesnt actually do anything
+#because it doesnt get the key for the 
 def main():
     print("Dropping all tables...")
     Base.metadata.drop_all(engine)
-    
+    print(Base.metadata.tables.keys())  # shows all tables it knows about 
     print("Creating tables with new schema...")
     init_db()
     
